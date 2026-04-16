c68a699496984fe5bdc7544fa7e8742466055868
surenss861 <ssurn29@gmail.com>
2026-04-16 03:01:31 -0400

    Initial Commit

---
+++ puslefill/puslefillApp.swift
@@ -0,0 +1,32 @@
+//
+//  puslefillApp.swift
+//  puslefill
+//
+//  Created by suren sureshkumar on 2026-04-16.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct puslefillApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ puslefillUITests/puslefillUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  puslefillUITestsLaunchTests.swift
+//  puslefillUITests
+//
+//  Created by suren sureshkumar on 2026-04-16.
+//
+
+import XCTest
+
+final class puslefillUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
